7003c7f5e8e34fb0add2f1db00a506f11ed37a4a
Askhad Arzybaev <aarzybaev@gmail.com>
2024-04-18 20:30:37 +0600

    added new route

--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ import artistsRouter from "./routers/artists";
 import albumsRouter from "./routers/albums";
 import tracksRouter from "./routers/tracks";
 import config from "./config";
+import usersRouter from "./routers/users";
+import trackHistoriesRouter from "./routers/trackHistories";
 
 const app = express();
 const port = 8000;
@@ -15,6 +17,8 @@ app.use(express.static('public'));
 app.use('/artists', artistsRouter);
 app.use('/albums', albumsRouter);
 app.use('/tracks', tracksRouter);
+app.use('/users', usersRouter);
+app.use('/trackHistories', trackHistoriesRouter);
 
 
 const run = async () => {
