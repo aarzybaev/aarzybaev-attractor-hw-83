d928846528facda6317413b02d0b6f498fbf3889
Askhad Arzybaev <aarzybaev@gmail.com>
2024-04-18 20:29:20 +0600

    added types for models

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,5 @@
+import mongoose, {Model} from "mongoose";
+
 export interface artistsApi {
     _id: string;
     name: string;
@@ -19,4 +21,42 @@ export interface tracksApi {
     title: string;
     album: string;
     duration: string;
+}
+
+export interface ArtistFields {
+    name: string;
+    photo: string | null;
+    information: string;
+}
+
+export interface AlbumFields {
+    title: string;
+    artist: mongoose.Types.ObjectId;
+    releaseDate: string;
+    coverImage: string | null;
+}
+
+export interface TrackFields {
+    title: string;
+    album: mongoose.Types.ObjectId;
+    duration: string;
+}
+
+export interface UserFields {
+    username: string;
+    password: string;
+    token: string;
+}
+
+export interface UserMethods {
+    checkPassword(password: string): Promise<boolean>;
+    generateToken(): void;
+}
+
+export type UserModel = Model<UserFields, {}, UserMethods>;
+
+export interface TrackHistoryFields {
+    user: string;
+    track: string;
+    datetime: Date;
 }
