396527b37c23d42e4452c2b7f2974133c2bd320d
Askhad Arzybaev <aarzybaev@gmail.com>
2024-04-18 20:26:40 +0600

    create user model

---
+++ models/User.ts
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+import {UserFields, UserMethods, UserModel} from "../types";
+import config from "../config";
+import {randomUUID} from "crypto";
+
+const Schema = mongoose.Schema;
+const SALT_WORK_FACTOR = 10;
+
+
+
+const UserSchema = new Schema<UserFields, UserModel, UserMethods>({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    token: {
+        type: String,
+        required: true,
+    }
+}, config.mongoose.versionKey);
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    const hash = await bcrypt.hash(this.password, salt);
+    this.password = hash;
+    next();
+});
+
+UserSchema.set('toJSON', {
+    transform: (doc, ret, options) => {
+        delete ret.password;
+        return ret;
+    }
+});
+
+UserSchema.methods.checkPassword = function(password: string) {
+    return bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateToken = function() {
+    this.token = randomUUID();
+};
+
+const User = mongoose.model<UserFields, UserModel>('User', UserSchema);
+
+export default User;
